ded908645c132312756a4ae9e09cf4c4bcf31bc8
Anand Golla <anand.golla@ggktech.com>
2019-01-21 17:49:36 +0530

    Included login and add customer screens

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+// db.js
+
+import Firebase from 'firebase';
+ let config = {
+  apiKey: "AIzaSyBIeAUT1Tjku0lN8RAfLD23MoX0vHQV8WA",
+  authDomain: "chitti-a47d0.firebaseapp.com",
+  databaseURL: "https://chitti-a47d0.firebaseio.com",
+  projectId: "chitti-a47d0",
+  storageBucket: "",
+  messagingSenderId: "37934067421"
+  };
+let app = Firebase.initializeApp(config);
+export const db = app.database();
---
+++ src/screens/home.js
@@ -0,0 +1,57 @@
+// Home.js
+
+import React, { Component } from 'react';
+import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
+
+export default class Home extends Component {
+
+  _onPressAddCustomerButton=() => {
+    this.props.navigation.navigate('AddCustomer');
+    //Alert.alert('You tapped the Login!')
+  }
+
+  render() {
+    return (
+      <View style={styles.main}>
+        <TouchableHighlight
+                style = {styles.button}
+                underlayColor= "white"
+                onPress = {this._onPressAddCustomerButton}
+              >
+              <Text
+                  style={styles.buttonText}>
+                  Add Customer
+              </Text>
+            </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+main: {
+  flex: 1,
+  padding: 30,
+  flexDirection: 'column',
+  justifyContent: 'center',
+  backgroundColor: '#2a8ab7'
+},
+buttonText: {
+  fontSize: 18,
+  color: '#111',
+  alignSelf: 'center'
+},
+button: {
+  height: 45,
+  flexDirection: 'row',
+  backgroundColor:'white',
+  borderColor: 'white',
+  borderWidth: 1,
+  borderRadius: 8,
+  marginBottom: 10,
+  marginTop: 10,
+  alignSelf: 'stretch',
+  justifyContent: 'center'
+}
+});
