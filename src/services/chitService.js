62669db964a6dd571e4911133c0566b40fad66d8
Anand Golla <anand.golla@ggktech.com>
2019-01-23 18:36:45 +0530

    Saved time stamp to Chit table

--- src/services/chitService.js
+++ src/services/chitService.js
@@ -2,10 +2,12 @@
 
 import { db } from '../config/db';
 
+Date.prototype.getUnixTime = function() { return this.getTime()/1000|0 };
+
 export const addChit =  (name, date, duration) => {
     db.ref('/Chit').push({
         name: name,
-        startDate: date,
+        startDate: date.getUnixTime(),
         duration: duration
     });
 }
