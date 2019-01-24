35c1f050783c33c372654f1e2c93833cd6178756
Anand Golla <anand.golla@ggktech.com>
2019-01-24 18:07:23 +0530

    Implemented tabbar controller

--- App.js
+++ App.js
@@ -7,34 +7,145 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {createStackNavigator, createAppContainer} from 'react-navigation';
+import {createStackNavigator, createAppContainer, createBottomTabNavigator} from 'react-navigation';
+import { View, StyleSheet, AppState, Modal } from 'react-native';
+import PasswordGesture from 'react-native-gesture-password'
 
 import LockScreen from './src/screens/lock'
 import LoginScreen from './src/screens/login'
-import HomeScreen from './src/screens/home'
 import AddCustomerScreen from './src/screens/addCustomer'
 import ViewCustomerScreen from './src/screens/viewCustomer'
 import AddChitScreen from './src/screens/addChit'
 
-const RootStack = createStackNavigator(
+// const RootStack = createStackNavigator(
+//   {
+//     Lock: LockScreen,
+//     Login: LoginScreen,
+//     Home: HomeScreen,
+//     AddCustomer: AddCustomerScreen,
+//     ViewCustomer: ViewCustomerScreen,
+//     AddChit: AddChitScreen
+//   },
+//   {
+//     initialRouteName: 'Home',
+//   }
+// );
+
+const CustomerStack = createStackNavigator(
   {
-    Lock: LockScreen,
-    Login: LoginScreen,
-    Home: HomeScreen,
-    AddCustomer: AddCustomerScreen,
     ViewCustomer: ViewCustomerScreen,
-    AddChit: AddChitScreen
+    AddCustomer: AddCustomerScreen,
   },
   {
-    initialRouteName: 'Lock',
+    initialRouteName: 'ViewCustomer',
   }
 );
 
-const AppContainer = createAppContainer(RootStack);
+const ChitStack = createStackNavigator(
+  {
+    // ViewCustomer: ViewCustomerScreen,
+    AddChit: AddChitScreen,
+  },
+  {
+    initialRouteName: 'AddChit',
+  }
+);
+
+const customerContainer = createAppContainer(CustomerStack);
+
+const TabNavigator = createBottomTabNavigator({
+  Home: { screen: ChitStack },
+  ViewCustomer: { screen: customerContainer },
+});
+
+const AppContainer = createAppContainer(TabNavigator);
+
 
 export default class App extends Component {
+
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  componentDidMount() {
+    AppState.addEventListener('change', this._handleAppStateChange);
+  }
+
+  _handleAppStateChange = (nextAppState) => {
+    if (nextAppState === 'active') {
+      this.setModalVisible(true);
+    }
+  }
+
+  onStart = () => {
+    this.setState({
+        status: 'normal',
+        message: 'Please input your password.'
+    });
+  }
+
+  onEnd = (password) => {
+      if (password == '123') {
+        this.setState({
+            status: 'right',
+            message: 'Password is right, success.'
+        });
+        this.setModalVisible(false);
+
+      } else {
+        this.setState({
+            status: 'wrong',
+            message: 'Password is wrong, try again.'
+        });
+      }
+  }
+
+  onReset = () => {
+    this.setState({
+        status: 'normal',
+        message: 'Please input your password.'
+    });
+  }
+
   render() {
-    return <AppContainer />;
+    return (
+      <View style={styles.main}>
+      <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 22}}>
+            <View>
+            <PasswordGesture
+                ref='pg'
+                status={this.state.status}
+                message={this.state.message}
+                onStart={() => this.onStart()}
+                onEnd={(password) => this.onEnd(password)}
+                onReset={() => this.onReset()}
+                />
+            </View>
+          </View>
+        </Modal>
+
+        <AppContainer />
+      </View>
+    )
+    // return <AppContainer />;
   }
 }
+
+const styles = StyleSheet.create({
+main: {
+  flex: 1,
+  padding: 0,
+  backgroundColor: '#2a8ab7'
+}
+});
