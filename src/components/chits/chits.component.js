5d466523c52ce65da9cc6f5bd60bcc90cbcbf88c
Anand Golla <anand.golla@ggktech.com>
2019-02-26 15:57:03 +0530

    Implemented renderItem method

--- src/components/chits/chits.component.js
+++ src/components/chits/chits.component.js
@@ -1,152 +1,173 @@
 
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList, ActivityIndicator, TouchableHighlight, Button} from 'react-native';
+import { View, Text, StyleSheet, FlatList, ActivityIndicator, TouchableHighlight, Button } from 'react-native';
 import { List, ListItem, SearchBar } from 'react-native-elements'
 import Swipeout from 'react-native-swipeout';
-import {updateFilteredChitList, updateAllChitList} from './chits.actions'
+import { updateFilteredChitList, updateAllChitList } from './chits.actions'
 
 import { db } from '../../config/db';
 
 let itemsRef = db.ref('/Chit');
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      backgroundColor: '#B6A6BB',
-    },
-    button: {
-      height: 45,
-      width: 100
-    }
-  })
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#B6A6BB',
+  },
+  button: {
+    height: 45,
+    width: 100
+  }
+})
 
-  export default class Chits extends Component {
+export default class Chits extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            loading: false,
-            error: null,
-        };
-    }
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: false,
+      error: null,
+    };
+  }
 
-    componentDidMount() {
+  componentDidMount() {
 
-      this.props.navigation.setParams({ addButtonClicked: this._addButtonClicked });
+    this.props.navigation.setParams({ addButtonClicked: this._addButtonClicked });
 
-        this.setState({ loading: true });
-        itemsRef.on('value', (snapshot) => {
-            let error = snapshot.error
-            var items = [];
-            snapshot.forEach((child) => {
-              items.push({
-                name: child.val().name,
-                startDate: child.val().startDate,
-                duration: child.val().duration,
-                key: child.key
-              });
-            });
+    this.setState({ loading: true });
+    itemsRef.on('value', (snapshot) => {
+      let error = snapshot.error
+      var items = [];
+      snapshot.forEach((child) => {
+        items.push({
+          name: child.val().name,
+          startDate: child.val().startDate,
+          duration: child.val().duration,
+          key: child.key
+        });
+      });
 
-            this.props.updateFilteredChitList(items)
-            this.props.updateAllChitList(items)
-            this.setState({
-                loading: false,
-                error: error,
-            });
-         });
-    }
+      this.props.updateFilteredChitList(items)
+      this.props.updateAllChitList(items)
+      this.setState({
+        loading: false,
+        error: error,
+      });
+    });
+  }
 
-    configureScene() {
-      return ExNavigator.SceneConfigs.FloatFromRight;
-    }
+  configureScene() {
+    return ExNavigator.SceneConfigs.FloatFromRight;
+  }
 
-    renderSeparator = () => {
-        return (
-          <View
-            style={{
-              height: 1,
-              width: "95%",
-              backgroundColor: "#CED0CE",
-              marginLeft: "5%"
-            }}
-          />
-        );
-      };
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "95%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "5%"
+        }}
+      />
+    );
+  };
 
-      renderHeader = () => {
-        return (
-          <SearchBar
-            placeholder="Search Chit name..."
-            lightTheme
-            round
-            onChangeText={text => this.props.searchFilterFunction(text, this.props.allChitList)}
-            autoCorrect={false}
-          />
-        );
-      };
+  renderHeader = () => {
+    return (
+      <SearchBar
+        placeholder="Search Chit name..."
+        lightTheme
+        round
+        onChangeText={text => this.props.searchFilterFunction(text, this.props.allChitList)}
+        autoCorrect={false}
+      />
+    );
+  };
 
-      static navigationOptions = ({ navigation }) => {
-        return {
-          title: 'Chits',
-          headerRight: (
-            <Button
-              title='Add'
-              onPress={navigation.getParam('addButtonClicked')}
-              />
-          ),
-        };
-      };
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Chits',
+      headerRight: (
+        <Button
+          title='Add'
+          onPress={navigation.getParam('addButtonClicked')}
+        />
+      ),
+    };
+  };
 
-      _addButtonClicked = () => {
-        this.props.navigation.navigate('AddChit');
-      };
+  _addButtonClicked = () => {
+    this.props.navigation.navigate('AddChit');
+  };
 
-      onPressListItem = (item) => {
-        console.log('Item clicked')
-        this.props.navigation.navigate('ViewCustomer', {item:item});
-      }
-    
-    render() {
+  onPressListItem = (item) => {
+    this.props.navigation.navigate('ViewCustomer', { item: item });
+  }
 
-      var swipeoutBtns = [
-        {
-          text: 'Delete'
-        },
-        {
-          text: 'Call'
+  removeChit = (item) => {
+    // if (this.screenFor == "AllCustomers") {
+    //   db.ref('/Customers').child(item.key).remove();
+    // } else {
+    //   removeCustomerFromChit(this.props.navigation.state.params.item.key, item.key)
+
+    //   var items = this.state.items
+    //   var index = items.indexOf(item);
+
+    //   items.splice(index, 1)
+    //   this.setState({
+    //     items: items
+    //   })
+    //   this.arrayholder.pop(item)
+    // }
+  }
+
+  renderRow(item) {
+    var swipeoutBtns = [
+      {
+        text: 'Delete',
+        backgroundColor: 'red',
+        onPress: () => {
+          this.removeChit(item)
         }
-      ]
+      }
+    ]
 
-        if (this.state.loading) {
-            return (
-              <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <ActivityIndicator />
-              </View>
-            );
-          }
-        return (
-            <List containerStyle={{borderTopWidth: 0, borderBottomWidth: 0}}>
-                <FlatList
-                    data={this.props.filteredChitList}
-                    renderItem={({ item }) => (
-                      <Swipeout right={swipeoutBtns}>
+    return (
+      <Swipeout right={swipeoutBtns}
+        autoClose='true'
+        backgroundColor='transparent'>
+        <ListItem
+          key={item.key}
+          title={item.name}
+          subtitle={item.duration}
+          containerStyle={{ borderBottomWidth: 0, borderTopWidth: 0 }}
+          onPress={() => { this.onPressListItem(item) }}
+        />
+      </Swipeout>
+    )
+  }
 
-                        <ListItem
-                        key={item.key}
-                        title={item.name}
-                        subtitle={item.duration}
-                        containerStyle={{borderBottomWidth: 0, borderTopWidth: 0}}
-                        onPress={()=> { this.onPressListItem(item)}}
-                        />
-                        </Swipeout>
+  render() {
 
-                    )}
-                    keyExtractor={item => item.key}
-                    ItemSeparatorComponent={this.renderSeparator}
-                    ListHeaderComponent={this.renderHeader}
-                    />
-            </List>
-        )
+    if (this.state.loading) {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <ActivityIndicator />
+        </View>
+      );
     }
+    return (
+      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+        <FlatList
+          data={this.props.filteredChitList}
+          renderItem={({ item }) => (this.renderRow(item))}
+          keyExtractor={item => item.key}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+        />
+      </List>
+    )
+  }
 }
